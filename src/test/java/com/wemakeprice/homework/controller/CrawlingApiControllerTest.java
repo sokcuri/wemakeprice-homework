68026b7157f3412ef2800dcb5ec11158d91e7fde
joshua-qa <jgchoi.qa@gmail.com>
2020-04-24 17:52:35 +0900

    * TextUtilsTest 클래스에 출력 묶음 단위 테스트 추가, ApiControllerTest 작성

---
+++ src/test/java/com/wemakeprice/homework/controller/CrawlingApiControllerTest.java
@@ -0,0 +1,73 @@
+package com.wemakeprice.homework.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wemakeprice.homework.domain.CrawlingApiRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CrawlingApiControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper;
+
+    private MediaType contentType;
+
+    @BeforeEach
+    public void before() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true))
+                .alwaysDo(print())
+                .build();
+
+        mapper = new ObjectMapper();
+
+        contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+                MediaType.APPLICATION_JSON.getSubtype(),
+                Charset.forName("utf8"));
+    }
+
+    @Test
+    public void 컨트롤러_성공_케이스() throws Exception {
+        CrawlingApiRequest request = makeApiRequest("https://httpstatusdogs.com/418-im-a-teapot", "exclude_html", 50);
+
+        ResultActions result = mockMvc.perform(
+                post("http://localhost:8080/api/crawling")
+                .contentType(contentType)
+                .content(mapper.writeValueAsString(request))
+        ).andDo(print());
+
+        result.andExpect(status().isOk());
+    }
+
+    private CrawlingApiRequest makeApiRequest(String url, String type, int count) {
+        CrawlingApiRequest request = new CrawlingApiRequest();
+
+        request.setUrl(url);
+        request.setType(type);
+        request.setOutputUnitCount(count);
+
+        return request;
+    }
+}
--- src/test/java/com/wemakeprice/homework/util/TextUtilsTest.java
+++ src/test/java/com/wemakeprice/homework/util/TextUtilsTest.java
@@ -1,22 +1,37 @@
 package com.wemakeprice.homework.util;
 
-import com.wemakeprice.homework.enums.TextType;
+import com.wemakeprice.homework.domain.DivisionText;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TextUtilsTest {
 
-    private String shortEnglishText = "asdfFdAatest";
-    private String longEnglishText = "wwZaBnjtestbbbMALLLLLLLLLLLLpoiiIcef";
+    private String shortAlphabetText = "asdfFdAatest";
+    private String longAlphabetText = "LoremIpsumissimplydummytextoftheprintingandtypesettingindustryLoremIpsumhasbeentheindustrysstandarddummytexteversincetheswhenanunknownprintertookagalleyoftypeandscrambledittomakeatypespecimenbookIthassurvivednotonlyfivecenturiesbutalsotheleapintoelectronictypesettingremainingessentiallyunchangedItwaspopularisedintheswiththereleaseofLetrasetsheetscontainingLoremIpsumpassagesandmorerecentlywithdesktoppublishingsoftwarelikeAldusPageMakerincludingversionsofLoremIpsum";
     private String shortNumericText = "13579086";
     private String longNumericText = "135790864222123";
 
     @Test
     public void 영문_숫자_한글자씩조합() {
-        String sortedEnglishText = TextUtils.getSortedText(shortEnglishText, TextType.ALPHABET);
-        String sortedNumericText = TextUtils.getSortedText(shortNumericText, TextType.NUMERIC);
+        String sortedAlphabetText = TextUtils.getSortedAlphabetText(shortAlphabetText);
+        String sortedNumericText = TextUtils.getSortedNumericText(shortNumericText);
 
-        assertThat(sortedEnglishText).isEqualTo("AaaddeFfsstt");
+        assertThat(sortedAlphabetText).isEqualTo("AaaddeFfsstt");
+        assertThat(sortedNumericText).isEqualTo("01356789");
+    }
+
+    @Test
+    public void 출력묶음단위_테스트() {
+        String sortedAlphabetText = TextUtils.getSortedAlphabetText(longAlphabetText);
+        String sortedNumericText = TextUtils.getSortedNumericText(longNumericText);
+
+        String mixedText = TextUtils.getMixedText(sortedAlphabetText, sortedNumericText);
+        int outputUnitCount = 50;
+        DivisionText unit = TextUtils.getUnit(mixedText, outputUnitCount);
+
+        assertThat(unit.getQuota()).isEqualTo("A0a1a1a2a2a2a2a3a3a4a5a6a7a8a9aaaaaaaaaaaaaabbbbbccccccccccddddddddddddddddeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeffffffggggggggggghhhhhhhhhhhhhhIIIIIIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiikkkkkkkLLLLLlllllllllllllllllMmmmmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnoooooooooooooooooooooooooPpppppppppppppppppprrrrrrrrrrrrrrrrrrrrrrrrssssssssssssssssssssssssssssssssssssssstttttttttttttttttttttttttttttttttttttttttttuuuuuuuuuuu");
+        assertThat(unit.getRemainder()).isEqualTo("uuuuuuvvvvvwwwwwwxxyyyyyyyyyyyyy");
+        assertThat(unit.getRemainder()).hasSizeLessThan(outputUnitCount);
     }
 }

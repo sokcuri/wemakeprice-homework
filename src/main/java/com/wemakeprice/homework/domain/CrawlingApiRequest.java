40db550805d6c93b0c1b288b2bc67e0d63d06910
joshua-qa <jgchoi.qa@gmail.com>
2020-04-24 14:57:21 +0900

    ” Enum Validator 작업 완료 및 request type 필드에 적용

--- src/main/java/com/wemakeprice/homework/domain/CrawlingApiRequest.java
+++ src/main/java/com/wemakeprice/homework/domain/CrawlingApiRequest.java
@@ -1,5 +1,7 @@
 package com.wemakeprice.homework.domain;
 
+import com.wemakeprice.homework.enums.ParseOption;
+import com.wemakeprice.homework.validator.annotation.EnumValue;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.URL;
@@ -14,6 +16,7 @@ public class CrawlingApiRequest {
     @URL
     private String url;
 
+    @EnumValue(enumClass = ParseOption.class, message = "유효하지 않은 type입니다.")
     private String type;
 
     private int outputUnitCount;
--- src/main/java/com/wemakeprice/homework/validator/EnumValidator.java
+++ src/main/java/com/wemakeprice/homework/validator/EnumValidator.java
@@ -1,19 +1,30 @@
 package com.wemakeprice.homework.validator;
 
 import com.wemakeprice.homework.validator.annotation.EnumValue;
+import org.springframework.util.StringUtils;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EnumValidator implements ConstraintValidator<EnumValue, CharSequence> {
 
+    private List<String> allowValues;
+
     @Override
     public void initialize(EnumValue constraintAnnotation) {
-
+        allowValues = Stream.of(constraintAnnotation.enumClass().getEnumConstants())
+                .map(Enum::name).collect(Collectors.toList());
     }
 
     @Override
     public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
-        return false;
+        if (StringUtils.isEmpty(value)) {
+            return true;
+        }
+
+        return allowValues.contains(value.toString());
     }
 }

0e196f356a624eba18f9f82b2a6a60b3e7db9cea
joshua-qa <jgchoi.qa@gmail.com>
2020-04-24 14:51:03 +0900

    * Request Validation 일부 적용 (URL annotation 사용)

--- src/main/java/com/wemakeprice/homework/domain/CrawlingApiRequest.java
+++ src/main/java/com/wemakeprice/homework/domain/CrawlingApiRequest.java
@@ -2,6 +2,7 @@ package com.wemakeprice.homework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 /**
  * 크롤링 API 리퀘스트용 객체
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 public class CrawlingApiRequest {
 
+    @URL
     private String url;
 
     private String type;
